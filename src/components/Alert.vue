08c76b6ce4eacfa970c405c6ed2b8c3a59f6a060
yoelnacho <nacho.test.developer@gmail.com>
2019-11-05 11:09:26 -0300

    Alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="alert alert-success fade show">
+    <span class="close" data-dismiss="alert">x</span>
+    <slot/>
+    <button type="button" class="btn btn-primary btn-lg">Close</button>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import wrap from '@vue/web-component-wrapper';
-import MyWebComponent from './components/MyWebComponent';
+// import MyWebComponent from './components/MyWebComponent';
 import Button from './components/Button';
+import Alert from './components/Alert';
 
-const WrappedElement = wrap(Vue, MyWebComponent);
+// const WrappedElement = wrap(Vue, MyWebComponent);
 const WrappedButton = wrap(Vue, Button);
+const WrappedAlert = wrap(Vue, Alert);
 
-window.customElements.define('my-web-component', WrappedElement);
+// window.customElements.define('my-web-component', WrappedElement);
 window.customElements.define('ceg-button', WrappedButton);
+window.customElements.define('ceg-alert', WrappedAlert);
